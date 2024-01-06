6b5eebf2489ee8b446f412dcd1cc7508e9637358
merga wolde <49088728+merga-wolde@users.noreply.github.com>
2024-01-06 20:45:58 +0000

    python more class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+File: 0-rectangle.py
+Desc: This file contains a simple class defination called Rectangle
+
+"""
+
+
+class Rectangle():
+    """
+    This is an empty class defination called Rectangle.
+    """
+    pass
